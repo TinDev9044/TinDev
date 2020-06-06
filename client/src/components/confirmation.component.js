1471715e4615de890247e0b0ed5f39b2d4a5b415
JafarAbbas <abbasrafaj@gmail.com>
2020-06-06 21:35:30 +0530

    asd

--- client/src/components/confirmation.component.js
+++ client/src/components/confirmation.component.js
@@ -4,48 +4,53 @@ export default class Confirmation extends Component {
     constructor(props) {
       super(props);
   //binding so that this can be used
-      this.onChangeOtp = this.onChangeOtp.bind(this);
+      this.onChangeemail = this.onChangeemail.bind(this);
+      this.onChangePassword = this.onChangePassword.bind(this);
       this.onSubmit = this.onSubmit.bind(this);
       this.state = {
-        otp: ''
+        email: '',
+        password: ''
       }
     }
-    onChangeOtp(e) {
+    onChangeemail(e) {
         this.setState({
-          otp: e.target.value
+          email: e.target.value
         })
       }
+      onChangePassword(e) {
+        this.setState({
+          password: e.target.value
+        })
+      }
+    
       onSubmit(e) {
         e.preventDefault();
     
-        const Otp = {
-          otp: this.state.otp
+        const user = {
+          email: this.state.email,
+          password: this.state.password
         }
     
-        console.log(Otp);
+        console.log(user);
     
-        axios.post('https://tindev9044.herokuapp.com/confirmation/', Otp)
-          .then(res => {
-
-            if (res.status == 200) {
-              window.location = "/user"
-          } 
-      })
-      .catch(function(error) {
-          window.location = "/confirmation"
-      })
-
-          }
-      
+        axios.post('https://tindev9044.herokuapp.com/confirmation/', user)
+          .then(res => console.log(res.data));
+      }
 render(){
     return(<div>
         <form onSubmit={this.onSubmit}>
             <input type="text" 
             required
                 //setting the email to blank
-                value={this.state.otp}
+                value={this.state.email}
                 //changing the value of state.email
-                onChange={this.onChangeOtp}></input>
+                onChange={this.onChangeemail}></input>
+            <input type="password"
+            required
+                //setting the email to blank
+                value={this.state.password}
+                //changing the value of state.email
+                onChange={this.onChangePassword}></input>
                 <input type="submit"></input>
         </form>
         
