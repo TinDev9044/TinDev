b715eed0275bfe04ee1e04c110961ba721106936
JafarAbbas <abbasrafaj@gmail.com>
2020-06-08 11:23:26 +0530

    Gaurav type 500 400  msgs push

--- routes/confirmation.js
+++ routes/confirmation.js
@@ -9,13 +9,13 @@ router.route('/').post((req, res, next) => {
  
         // If we found a token, find a matching user
         User.findOne({ _id: token._userId}, function (err, user) {
-            if (!user) return res.status(400).send({ msg: 'We were unable to find a user for this token.' });
-            if (user.isVerified) return res.status(400).send({ type: 'already-verified', msg: 'This user has already been verified.' });
+            if (!user) return res.status(500).send({type: 500, msg: 'We were unable to find a user for this token.' });
+            if (user.isVerified) return res.status(400).send({ type: 400, msg: 'This user has already been verified.' });
  
             // Verify and save the user
             user.isVerified = true;
             user.save(function (err) {
-                if (err) { return res.status(500).send({ msg: err.message }); }
+                if (err) { return res.status(500).send({  type: 400,msg: err.message }); }
                 return(res.status(200).send({ type:200,user:user,msg: "Your account has been verified." }));
             });
         });
